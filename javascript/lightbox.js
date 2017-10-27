d0511ea94aa529230367ace2ea804cb768d97bfb
Unknown <amandeepmalhi21@hotmail.com>
2017-10-27 12:18:40 -0700

    updated product for mens runner and duplicated

--- javascript/lightbox.js
+++ javascript/lightbox.js
@@ -30,9 +30,9 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += "active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
