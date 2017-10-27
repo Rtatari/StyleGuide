38b7f05f02a7de2b823031114f317d4084e30119
rambod <rambod.tatari@gmail.com>
2017-10-27 14:56:26 -0700

    styleGuide updated

--- javascript/lightbox.js
+++ javascript/lightbox.js
@@ -33,6 +33,6 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  // dots[slideIndex-1].className += "active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
 }
